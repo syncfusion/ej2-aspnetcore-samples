9f0bf8747a010575eb88a4c9e5ad8351586b0c86
SyncfusionBuild <buildautomation@syncfusion.com>
2025-03-27 11:37:32 +0530

    v29.1.33

--- wwwroot/scripts/Progressbar/theme-color.js
+++
@@ -1,7 +0,0 @@
-﻿function loadProgressBarTheme(args) {
-    var selectedTheme = location.hash.split('/')[1];
-    selectedTheme = selectedTheme ? selectedTheme : 'Tailwind3';
-    args.progressBar.theme = (selectedTheme.charAt(0).toUpperCase() +
-        selectedTheme.slice(1)).replace(/-dark/i, 'Dark').replace(/contrast/i, 'Contrast').replace(/-highContrast/i, 'HighContrast');
-     return selectedTheme;
-}; 
--- wwwroot/scripts/SmithChart/theme-color.js
+++
@@ -1,5 +0,0 @@
-﻿function loadSmithChartTheme(args) {
-    let theme = location.hash.split('/')[1];
-    theme = theme ? theme : 'Tailwind3';
-    args.smithchart.theme = (theme.charAt(0).toUpperCase() + theme.slice(1).replace(/-dark/i, 'Dark').replace(/contrast/i, 'Contrast').replace(/-highContrast/i, 'HighContrast'));
-}

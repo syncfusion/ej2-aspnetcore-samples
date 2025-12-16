889858651ef32edef7b554fad2ada124716348d5
buildautomation <buildautomation@syncfusion.com>
2025-12-16 09:58:59 +0000

    v32.1.19

--- wwwroot/scripts/CircularChart3D/theme-color.js
+++
@@ -1,5 +0,0 @@
-﻿function loadCircular3DChartTheme(args) {
-    var selectedTheme = location.hash.split('/')[1];
-    selectedTheme = selectedTheme ? selectedTheme : 'Tailwind3';
-    args.chart.theme = (selectedTheme.charAt(0).toUpperCase() + selectedTheme.slice(1)).replace(/-dark/i, "Dark").replace(/contrast/i, 'Contrast').replace(/-highContrast/i, 'HighContrast');
-};
